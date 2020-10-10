81627ee6f937eb0b97e9bfb6110fe225b28eb47e
athul krishna <athulkrishnapv6@gmail.com>
2020-10-10 11:46:57 +0400

    some chnage done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-const request = require("request")
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https")
 
 
 
@@ -16,11 +17,47 @@ app.get('/', function(req , res) {
 })
 
 app.post("/",function(req ,res){
- var firstName = req.body.fName;
- var lastName = req.body.lName;
- var  email = req.body.email
+ const firstName = req.body.fName;
+ const lastName = req.body.lName;
+ const  email = req.body.email
 
- console.log(firstName, lastName, email)
+ const data = {
+   members: [
+     {
+       email_address: email,
+       status: "subscribed",
+       merge_field: {
+         FNAME: firstName,
+         LNAME: lastName
+       }
+     }
+   ]
+ }
+
+
+
+ const jsonData = JSON.stringify(data);
+ const url = "https://us2.api.mailchimp.com/3.0/lists/e29e5125ce";
+ const options = {
+   method: "POST",
+   auth: "athul1: 0d7896bae0c6648117cd6a1097977aa6-us2"
+
+ }
+
+
+  const request = https.request(url, options, function(){
+
+
+
+   response.on("data", function(data){
+     console.log(JSON.parse(data))
+   })
+
+   request.write(jsonData);
+   request.end();
+
+ })
+
 })
 
 
@@ -28,6 +65,15 @@ app.post("/",function(req ,res){
 
 
 
-app.listen(3000 , function(){
+app.listen(process.env.PORT, function(){
   console.log("server is running on port 3000")
 });
+
+
+
+// API key
+// 0d7896bae0c6648117cd6a1097977aa6-us2
+
+
+// List // IDEA:
+// e29e5125ce
